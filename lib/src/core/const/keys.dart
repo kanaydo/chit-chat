66a1d17be261af5249540aa765c3a4ea99068d75
kanaydo <batara.girsang@outlook.com>
2020-08-28 09:02:41 +0700

    initialize const key

---
+++ lib/src/core/const/keys.dart
@@ -0,0 +1,6 @@
+class Keys {
+  static const String APP_KEY = "APP_KEY";
+  static const String KEY_USER_ID = "${APP_KEY}_USER_ID";
+  static const String KEY_USER_TOKEN = "${APP_KEY}_USER_TOKEN";
+  static const String KEY_USER_LOGIN_STATUS = "${APP_KEY}_USER_LOGIN_STATUS";
+}
--- lib/src/core/session_manager.dart
+++ lib/src/core/session_manager.dart
@@ -1,31 +1,31 @@
+import 'package:flutter_base_app/src/core/const/keys.dart';
 import 'package:flutter_base_app/src/data/model/member.dart';
-import 'package:flutter_base_app/src/utils/const.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SessionManager {
 
   void setActiveMember(Member member) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setInt(MEMBER_ID_KEY, member.id);
-    prefs.setBool(MEMBER_LOGIN_STATUS_KEY, true);
+    prefs.setInt(Keys.KEY_USER_ID, member.id);
+    prefs.setBool(Keys.KEY_USER_LOGIN_STATUS, true);
   }
 
   Future<int> getActiveMember() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getInt(MEMBER_ID_KEY) ?? 0;
+    return prefs.getInt(Keys.KEY_USER_ID) ?? 0;
   }
 
   Future<bool> getLoggedInStatus() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(MEMBER_LOGIN_STATUS_KEY) ?? false;
+    return prefs.getBool(Keys.KEY_USER_LOGIN_STATUS) ?? false;
   }
 
   Future<bool> logoutMember() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.remove(MEMBER_ID_KEY);
-    prefs.remove(MEMBER_LOGIN_STATUS_KEY);
+    prefs.remove(Keys.KEY_USER_ID);
+    prefs.remove(Keys.KEY_USER_LOGIN_STATUS);
     bool result = false;
-    if (prefs.containsKey(MEMBER_ID_KEY) && prefs.containsKey(MEMBER_LOGIN_STATUS_KEY)) {
+    if (prefs.containsKey(Keys.KEY_USER_ID) && prefs.containsKey(Keys.KEY_USER_LOGIN_STATUS)) {
       result = false;
     } else {
       return true;
