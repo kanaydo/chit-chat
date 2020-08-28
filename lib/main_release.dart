3d2b652298fcda272eaa74d918786f92eb75fd83
kanaydo <batara.girsang@outlook.com>
2020-08-28 09:23:09 +0700

    ignore .env.release file

---
+++ lib/main_release.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_base_app/src/core/routes.dart';
+import 'package:flutter_base_app/src/views/home/home_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future main() async {
+  await DotEnv().load('.env.release');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Base App',
+      routes: Routes.getRoutes(),
+      theme: ThemeData(
+        pageTransitionsTheme: PageTransitionsTheme(
+            builders: {
+              TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+              TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+            }
+        ),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
