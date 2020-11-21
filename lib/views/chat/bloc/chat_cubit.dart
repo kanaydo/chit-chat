afb8cd3062d4f67c84e992a4f199584e91520357
kanaydo <batara.girsang@outlook.com>
2020-11-21 23:53:17 +0700

    receive realtime new message from action cable

--- lib/views/chat/bloc/chat_cubit.dart
+++ lib/views/chat/bloc/chat_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:action_cable/action_cable.dart';
 import 'package:flutter_base_app/core/network/app_exception.dart';
 import 'package:flutter_base_app/core/session_manager.dart';
 import 'package:flutter_base_app/data/model/message.dart';
@@ -5,6 +6,7 @@ import 'package:flutter_base_app/data/repository/conversation_repository.dart';
 import 'package:flutter_base_app/data/responses/add_message_response.dart';
 import 'package:flutter_base_app/data/responses/messages_response.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:meta/meta.dart';
 part 'chat_state.dart';
 
@@ -13,6 +15,8 @@ class ChatCubit extends Cubit<ChatState> {
   var _conversationRepository = ConversationRepository();
   int _conversationId;
   int _userId;
+  List<Message> _messages = [];
+
   ChatCubit(int conversationId) : super(null) {
     _init();
     this._conversationId = conversationId;
@@ -24,13 +28,15 @@ class ChatCubit extends Cubit<ChatState> {
       this._userId = userId;
     }
     getConversationMessages();
+    connectToConversationChannel();
   }
 
   void getConversationMessages() async {
     emit(ChatLoading(message: 'getting messages..'));
     try {
       MessagesResponse messagesResponse = await _conversationRepository.conversationMessages(_conversationId);
-      emit(ChatIdle(messages: messagesResponse.messages, userId: _userId));
+      this._messages = messagesResponse.messages;
+      emit(ChatIdle(messages: _messages, userId: _userId));
     } on ApiException catch (api) {
       emit(ChatError(message: api.toString()));
     } catch (e) {
@@ -53,5 +59,29 @@ class ChatCubit extends Cubit<ChatState> {
     } catch (e) {
       print(e.toString());
     }
+  }
+
+  connectToConversationChannel() {
+    var cable = ActionCable.Connect(
+        DotEnv().env['CABLE'], onConnected: () {
+          print("connected to chat channel");
+        }
+    );
+    cable.subscribe(
+        "MessageNotificationChannel",
+        channelParams: {"conversation_id": _conversationId},
+        onSubscribed: () {},
+        onDisconnected: () {},
+        onMessage: (Map message) {
+          Message newMessage = Message.fromJSON(message);
+          incomingMessage(newMessage);
+          print(newMessage.content);
+        }
+    );
+  }
+
+  incomingMessage(Message message) {
+    _messages.insert(0, message);
+    emit(ChatIdle(messages: _messages, userId: _userId));
   }
 }
